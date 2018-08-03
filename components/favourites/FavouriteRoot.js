884c437ea6999592a831d4afa7e6e5acf7b6a1b7
balamanova <asema.balamanova777@gmail.com>
2018-08-03 12:14:09 +0600

    0308

--- components/favourites/FavouriteRoot.js
+++ components/favourites/FavouriteRoot.js
@@ -1,26 +1,33 @@
-import React, { Component } from "react";
-import { createStackNavigator } from "react-navigation";
-import MainUniversities from "../universities/MainUniversities";
-import FavouriteList from "./FavouriteList";
+import React, { Component } from 'react';
+import { createStackNavigator } from 'react-navigation';
+import MainUniversities from '../universities/MainUniversities';
+import FavouriteList from './FavouriteList';
 
 export default createStackNavigator(
-  {
-    FavouriteList: {
-      screen: FavouriteList,
-      navigationOptions: {
-        title: "Избранное",
-        headerTintColor: "white",
-        headerStyle: {
-          backgroundColor: "#F94040"
-        }
-      }
-    },
-    MainUniversities: {
-      screen: MainUniversities
-    }
-  },
-  {
-    headerBackTitleVisible: true,
-    headerMode: "screen"
-  }
+	{
+		FavouriteList: {
+			screen: FavouriteList,
+			navigationOptions: {
+				title: 'Избранное',
+				headerTintColor: 'white',
+				headerStyle: {
+					backgroundColor: '#F94040',
+				},
+			},
+		},
+		DetailUniversities: {
+			screen: DetailUniversities,
+			navigationOptions: {
+				title: 'Университет',
+				headerTintColor: 'white',
+				headerStyle: {
+					backgroundColor: '#F94040',
+				},
+			},
+		},
+	},
+	{
+		headerBackTitleVisible: true,
+		headerMode: 'screen',
+	}
 );
