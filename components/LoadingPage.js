dae3128aeb25eb29b876a455562b9193a8ba4386
Akezhan <rakishev.akezhan@mail.ru>
2018-08-10 14:13:18 +0600

    Ok

--- components/LoadingPage.js
+++ components/LoadingPage.js
@@ -1,122 +1,122 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-	AppRegistry,
-	StyleSheet,
-	Text,
-	View,
-	Animated,
-	Image,
-	Easing,
-	AsyncStorage,
-	TouchableOpacity,
-	ActivityIndicator,
-	Platform,
-	Dimensions,
-	ImageBackground,
-} from 'react-native';
-import { Container } from 'native-base';
-import Mode from './ModeProvider';
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  Image,
+  Easing,
+  AsyncStorage,
+  TouchableOpacity,
+  ActivityIndicator,
+  Platform,
+  Dimensions,
+  ImageBackground
+} from "react-native";
+import { Container } from "native-base";
+import DataSave from "./DataSave";
 
-const { height } = Dimensions.get('window');
+const { height } = Dimensions.get("window");
 
 export default class Colors extends Component {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.RotateValueHolder = new Animated.Value(0);
-	}
+    this.RotateValueHolder = new Animated.Value(0);
+  }
 
-	componentWillMount() {
-		this.animatedValue = new Animated.Value(0);
-	}
-	componentDidMount() {
-		this.StartImageRotateFunction();
-		Animated.timing(this.animatedValue, {
-			toValue: 530,
-			duration: 3000,
-		}).start();
+  componentWillMount() {
+    this.animatedValue = new Animated.Value(0);
+  }
+  componentDidMount() {
+    this.StartImageRotateFunction();
+    Animated.timing(this.animatedValue, {
+      toValue: 530,
+      duration: 3000
+    }).start();
 
-		console.log('Mounted');
-	}
+    console.log("Mounted");
+  }
 
-	StartImageRotateFunction() {
-		this.RotateValueHolder.setValue(0);
+  StartImageRotateFunction() {
+    this.RotateValueHolder.setValue(0);
 
-		Animated.timing(this.RotateValueHolder, {
-			toValue: 1,
-			duration: 3000,
-			easing: Easing.linear,
-		}).start();
-	}
+    Animated.timing(this.RotateValueHolder, {
+      toValue: 1,
+      duration: 3000,
+      easing: Easing.linear
+    }).start();
+  }
 
-	render() {
-		const interpolateColor = this.animatedValue.interpolate({
-			inputRange: [0, 150],
-			outputRange: ['rgb(0,0,0)', 'rgb(51, 250, 170)'],
-		});
+  render() {
+    const interpolateColor = this.animatedValue.interpolate({
+      inputRange: [0, 150],
+      outputRange: ["rgb(0,0,0)", "rgb(51, 250, 170)"]
+    });
 
-		const RotateData = this.RotateValueHolder.interpolate({
-			inputRange: [0, 1],
-			outputRange: ['0deg', '360deg'],
-		});
+    const RotateData = this.RotateValueHolder.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"]
+    });
 
-		const animatedStyle = {
-			backgroundColor: 'white',
-			// transform: [{ translateY: this.animatedValue }]
-		};
+    const animatedStyle = {
+      backgroundColor: "white"
+      // transform: [{ translateY: this.animatedValue }]
+    };
 
-		return (
-			<React.Fragment>
-				<Container>
-					<View style={styles.container}>
-						<Image
-							style={{
-								width: 297,
-								height: 139,
-								left: 40,
-							}}
-							source={require('../assets/logoBagy.png')}
-						/>
-						<Animated.Image
-							style={{
-								bottom: 5,
-								right: 80,
-								width: 128,
-								height: 146,
-								transform: [{ rotate: RotateData }],
-							}}
-							source={require('../assets/logoT.png')}
-						/>
-					</View>
-				</Container>
-				<View
-					style={{
-						backgroundColor: '#F94040',
-					}}
-				>
-					<Mode setTimer={() => this.props.setTimer()} />
-				</View>
-			</React.Fragment>
-		);
-	}
+    return (
+      <React.Fragment>
+        <Container>
+          <View style={styles.container}>
+            <Image
+              style={{
+                width: 297,
+                height: 139,
+                left: 40
+              }}
+              source={require("../assets/logoBagy.png")}
+            />
+            <Animated.Image
+              style={{
+                bottom: 5,
+                right: 80,
+                width: 128,
+                height: 146,
+                transform: [{ rotate: RotateData }]
+              }}
+              source={require("../assets/logoT.png")}
+            />
+          </View>
+        </Container>
+        <View
+          style={{
+            backgroundColor: "#F94040"
+          }}
+        >
+          <DataSave setTimer={() => this.props.setTimer()} />
+        </View>
+      </React.Fragment>
+    );
+  }
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F94040',
-		flexDirection: 'row',
-	},
-	box: {
-		width: '100%',
-		height: '100%',
-		position: 'absolute',
-	},
-	button: {
-		flex: 1,
-		alignItems: 'center',
-		backgroundColor: 'white',
-		padding: 10,
-	},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F94040",
+    flexDirection: "row"
+  },
+  box: {
+    width: "100%",
+    height: "100%",
+    position: "absolute"
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 10
+  }
 });
--- components/ModeProvider.js
+++ components/ModeProvider.js
@@ -1,150 +1,65 @@
-import React from 'react';
-import { View, Text, ActivityIndicator, AsyncStorage } from 'react-native';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
+import React from "react";
+import { View, Text, ActivityIndicator, AsyncStorage } from "react-native";
 
-const GET_BY_SUBJECT = gql`
-	{
-		allMajors {
-			id
-			name
-			index
-			description
-			object
-			subject
-		}
-
-		allUniversities {
-			id
-			name
-			address
-			city
-			description
-			email
-			phone
-			webSite
-			photo
-			numberOfStudents
-			majorPoints {
-				id
-				kazPoint
-				kazSelPoint
-
-				rusPoint
-				rusSelPoint
-				majorName
-				major {
-					id
-					subject
-				}
-			}
-		}
-		allMajorPoints {
-			id
-			kazPoint
-			kazSelPoint
-			rusPoint
-			rusSelPoint
-			major {
-				id
-				subject
-			}
-			majorIndex
-			university {
-				id
-			}
-		}
-		allTests {
-			id
-			name
-			photo
-			stars
-			questionNum
-		}
-	}
-`;
-export const ModeContext = React.createContext('light');
-
-export default class Mode extends React.Component {
-	static navigationOptions = {
-		header: null,
-	};
-	saveGlobal = data => {
-		global.data = {
-			...data,
-			allUniversities: data.allUniversities.map(university => ({
-				...university,
-				isFav: false,
-			})),
-		};
-
-		this.props.setTimer();
-
-		return <View />;
-	};
-
-	render() {
-		return (
-			<Query query={GET_BY_SUBJECT}>
-				{({ loading, data, error }) =>
-					error ? console.log(error) : loading ? <ActivityIndicator /> : this.saveGlobal(data)
-				}
-			</Query>
-		);
-	}
-}
+export const ModeContext = React.createContext("light");
 
 export class ModeProvider extends React.Component {
-	state = {
-		favouriteUniversID: [],
-	};
+  state = {
+    favouriteUniversID: []
+  };
 
-	retrieveData = async () => {
-		try {
-			const favouriteUniversID = await AsyncStorage.getItem('favouriteUnivers');
-			if (favouriteUniversID !== null) {
-				this.setState({
-					favouriteUniversID: JSON.parse(favouriteUniversID),
-				});
-			}
-		} catch (error) {
-			console.log('Error retrieving data', error);
-		}
+  retrieveData = async () => {
+    try {
+      const favouriteUniversID = await AsyncStorage.getItem("favouriteUnivers");
+      if (favouriteUniversID !== null) {
+        this.setState({
+          favouriteUniversID: JSON.parse(favouriteUniversID)
+        });
+      }
+    } catch (error) {
+      console.log("Error retrieving data", error);
+    }
 
-		console('In retrieve d');
-	};
-	componentDidMount() {
-		this.retrieveData();
-	}
+    console("In retrieve d");
+  };
+  componentDidMount() {
+    this.retrieveData();
+  }
 
-	changeFavourites = currentUniversity => {
-		const { favouriteUniversID } = this.state;
+  changeFavourites = currentUniversity => {
+    const { favouriteUniversID } = this.state;
 
-		const newFavouriteUniversIds = favouriteUniversID.includes(currentUniversity.id)
-			? favouriteUniversID.filter(univer => univer !== currentUniversity.id)
-			: [...favouriteUniversID, currentUniversity.id];
+    const newFavouriteUniversIds = favouriteUniversID.includes(
+      currentUniversity.id
+    )
+      ? favouriteUniversID.filter(univer => univer !== currentUniversity.id)
+      : [...favouriteUniversID, currentUniversity.id];
 
-		try {
-			this.setState({ favouriteUniversID: newFavouriteUniversIds }, () => {
-				AsyncStorage.setItem('favouriteUnivers', JSON.stringify(newFavouriteUniversIds));
-			});
-		} catch (error) {
-			console.log('error', error);
-		}
-		console.log('im g');
-	};
+    try {
+      this.setState({ favouriteUniversID: newFavouriteUniversIds }, () => {
+        AsyncStorage.setItem(
+          "favouriteUnivers",
+          JSON.stringify(newFavouriteUniversIds)
+        );
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
+    console.log("im g");
+  };
 
-	render() {
-		return (
-			<ModeContext.Provider
-				value={{
-					favouriteUniversID: this.state.favouriteUniversID,
-					changeFavourites: currentUniversity => this.changeFavourites(currentUniversity),
-					retrieveData: () => this.retrieveData(),
-				}}
-			>
-				{this.props.children}
-			</ModeContext.Provider>
-		);
-	}
+  render() {
+    return (
+      <ModeContext.Provider
+        value={{
+          favouriteUniversID: this.state.favouriteUniversID,
+          changeFavourites: currentUniversity =>
+            this.changeFavourites(currentUniversity),
+          retrieveData: () => this.retrieveData()
+        }}
+      >
+        {this.props.children}
+      </ModeContext.Provider>
+    );
+  }
 }
